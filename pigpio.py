5c89dda70242bdd413cf634c38b267d823e584d0
plenoir <lenoir.patrick@gmail.com>
2026-04-11 16:16:40 +0200

    #1 - Change this code to not construct the URL from user-controlled data.

--- pigpio.py
+++ pigpio.py
@@ -1,31 +1,43 @@
 import os
 import json
+import platform
 import requests
-from datetime import datetime  # Ajoute cette ligne
+from datetime import datetime
+from urllib.parse import urlparse
 
-def get_system_metrics():
+DOMAINS_ALLOWLIST = ["192.168.1.174"]
+
+def _get_system_metrics():
     """Récupère les métriques système (température, CPU, RAM)."""
-    temp = (
-        os.popen("vcgencmd measure_temp")
-        .readline()
-        .replace("temp=", "")
-        .replace("'C\n", "")
-    )
-    cpu = (
-        os.popen("top -bn1 | grep \"Cpu(s)\" | awk '{print $2 + $4}'")
-        .readline()
-        .strip()
-    )
-    ram = (
-        os.popen("free -m | awk 'NR==2{printf \"%.2f\", $3*100/$2 }'")
-        .readline()
-        .strip()
-    )
+    temp = 0
+    cpu = 0
+    ram = 0
+
+    # Récupère le nom du système d'exploitation
+    os_type = platform.system()
+
+    if os_type == "Linux":
+        temp = (
+            os.popen("vcgencmd measure_temp")
+            .readline()
+            .replace("temp=", "")
+            .replace("'C\n", "")
+        )
+        cpu = (
+            os.popen("top -bn1 | grep \"Cpu(s)\" | awk '{print $2 + $4}'")
+            .readline()
+            .strip()
+        )
+        ram = (
+            os.popen("free -m | awk 'NR==2{printf \"%.2f\", $3*100/$2 }'")
+            .readline()
+            .strip()
+        )
     # Remplace "demo" par la date actuelle
     cpu_date_maj = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return {"temperature": temp, "cpu_usage": cpu, "ram_usage": ram, "cpu_date_maj": cpu_date_maj}
 
-def send_to_home_assistant(data, config):
+def _send_to_home_assistant(data, config):
     """Envoie les données à Home Assistant."""
     url = config["home_assistant"]["url"]
     headers = {
@@ -33,19 +45,30 @@ def send_to_home_assistant(data, config):
         "content-type": "application/json",
     }
     payload = {"state": True, "attributes": data}
-    response = requests.post(url, headers=headers, json=payload)
+
+    if urlparse(url).hostname in DOMAINS_ALLOWLIST:
+        response = requests.post(url, headers=headers, json=payload)
+    else:
+        response = None
     return response
 
 def main():
     """Point d'entrée principal du script."""
-    with open("/home/pi/pigpio/config.json", "r", encoding="utf-8") as f:
-        config = json.load(f)
+    # Obtenir le répertoire courant du script exécuté
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    metrics = get_system_metrics()
-    response = send_to_home_assistant(metrics, config)
+    # Construire le chemin vers config.json
+    config_path = os.path.join(current_dir, "config.json")
 
-    print("Données envoyées :", metrics)
-    print("Réponse du serveur :", response.status_code, response.text)
+    with open(config_path, "r", encoding="utf-8") as f:
+        _config = json.load(f)
+
+    metrics = _get_system_metrics()
+    response = _send_to_home_assistant(metrics, _config)
+
+    if response:
+        print("Données envoyées :", metrics)
+        print("Réponse du serveur :", response.status_code, response.text)
 
 if __name__ == "__main__":
     main()
